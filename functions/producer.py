e2898ee06778b2d1c247a8fd874e3db4d8cbc848
woberdesaad <appelraptormann@gmail.com>
2019-01-31 18:36:07 +0000

    Fixing Everything

--- functions/producer.py
+++ functions/producer.py
@@ -7,10 +7,12 @@ import os
 def handler(event, context):
     queue_client = boto3.client("sqs")
     ec2_client = boto3.client("ec2")
+    queue_url = queue_client.get_queue_url(QueueName=os.environ["QUEUE_NAME"], QueueOwnerAWSAccountId=os.environ["ACCOUNT_ID"])
+
 
     # Get information on all instnaces
     response = ec2_client.describe_instances()
 
     # For each instance, write to queue
-    queue_client.send_message(QueueUrl=os.environ["QUEUE_URL"], MessageBody=str(response))
+    queue_client.send_message(QueueUrl=queue_url, MessageBody=str(response))
 
