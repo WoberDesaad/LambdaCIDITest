04bb1a981d460f0d4c32a0893fe1158a3e9558ed
woberdesaad <appelraptormann@gmail.com>
2019-01-31 14:52:25 +0000

    Improving Functionsgit statusgit status

---
+++ functions/consumer.py
@@ -0,0 +1,8 @@
+import json
+import datetime
+import os
+import boto3
+
+def handler(event, context):
+    sns_client = boto3.client('sns')
+    sns_client.publish(TopicArn=os.environ["TOPIC_ARN"], Message=event)
---
+++ functions/producer.py
@@ -0,0 +1,16 @@
+import json
+import datetime
+import boto3
+import os
+
+
+def handler(event, context):
+    queue_client = boto3.client("sqs")
+    ec2_client = boto3.client("ec2")
+
+    # Get information on all instnaces
+    response = ec2_client.describe_instances()
+
+    # For each instance, write to queue
+    queue_client.send_message(os.environ["QUEUE_URL"], response)
+
