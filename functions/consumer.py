294ff6e96cb686f66b4121c2fa459fb9e87eeefb
woberdesaad <appelraptormann@gmail.com>
2019-01-31 18:16:16 +0000

    Fixed Json not string

--- functions/consumer.py
+++ functions/consumer.py
@@ -5,4 +5,4 @@ import boto3
 
 def handler(event, context):
     sns_client = boto3.client('sns')
-    sns_client.publish(TopicArn=os.environ["SNS_TOPIC"], Message=event)
+    sns_client.publish(TopicArn=os.environ["SNS_TOPIC"], Message=json.dumps(event))
